97b79e5b81aedd56b59dacc1db180f11e180e6dc
m2lovric <m2lovric@gmail.com>
2022-07-31 22:43:46 +0200

    Add Nav and Button, update style

---
+++ src/components/ButtonNav.tsx
@@ -0,0 +1,14 @@
+interface ButtonProps {
+  path: string;
+  alt: string;
+}
+
+const ButtonNav = ({ path, alt }: ButtonProps) => {
+  return (
+    <button className='bg-zinc-700  m-5 w-12 h-12 flex items-center justify-center hover:bg-green-600 rounded-full hover:rounded-2xl transition-all duration-500 ease-linear'>
+      <img src={path} alt={alt} className='w-8 h-8' />
+    </button>
+  );
+};
+
+export default ButtonNav;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,17 @@
+import logo from '../assets/icons8-discord-new-48.svg';
+import ButtonNav from './ButtonNav';
+
 const Sidebar = () => {
-  return <aside className='w-20 h-full bg-gray-800'>Sidebar</aside>;
+  return (
+    <nav className='w-20 h-full bg-gray-800'>
+      <ul className='list-none w-full flex flex-col items-center mt-5 '>
+        <button className='bg-indigo-600 rounded-2xl w-14 h-14 flex items-center justify-center'>
+          <img src={logo} alt='logo' className='w-9 h-9' />
+        </button>
+        <ButtonNav path={logo} alt='logo' />
+      </ul>
+    </nav>
+  );
 };
 
 export default Sidebar;
